1e946596df2d8e3b6568d1b3f2217c3149181a0d
Jimmy James <64377109+JimmyJames404@users.noreply.github.com>
2021-09-15 17:45:16 -0500

    Add files via upload

---
+++ FlatLoginWatermark/Models/Users.cs
@@ -0,0 +1,16 @@
+using Micron;
+using System;
+using System.Collections.Generic;
+
+namespace Data.Models
+{
+/***USER MODEL***/
+  [Table("users")]
+ public partial class User : IMicron
+ {
+        public Int32 ID {get; set;}
+        public String Name {get; set;}
+        public String Email {get; set;}
+        public String Password {get; set;}
+ }
+}
